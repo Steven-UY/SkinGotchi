f3d5107d6f1f17833988b0003c132cf946673864
Steven Uy <steven.uy10@gmail.com>
2024-06-17 12:50:59 +0800

    setup firebase config

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,13 @@
-console.log('hello from index.js');
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCgQq7oJBQmva2xKUj_TuLsB7CS4ZunoZY",
+   authDomain: "skincare-analyzer.firebaseapp.com",
+   projectId: "skincare-analyzer",
+   storageBucket: "skincare-analyzer.appspot.com",
+   messagingSenderId: "942303322650",
+   appId: "1:942303322650:web:e0fb8d0b1da5272ce867a3",
+   measurementId: "G-92QQXCXELJ"
+ };
+
+initializeApp(firebaseConfig)
