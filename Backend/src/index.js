940bbc8ccf53af97d31e90eee7025a465bb7cdd3
Steven Uy <steven.uy10@gmail.com>
2024-06-17 12:39:47 +0800

    setup webpack

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,17 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; 
-
-//Returns Firebase App instance 
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyCgQq7oJBQmva2xKUj_TuLsB7CS4ZunoZY",
-    authDomain: "skincare-analyzer.firebaseapp.com",
-    projectId: "skincare-analyzer",
-    storageBucket: "skincare-analyzer.appspot.com",
-    messagingSenderId: "942303322650",
-    appId: "1:942303322650:web:e0fb8d0b1da5272ce867a3",
-    measurementId: "G-92QQXCXELJ"
-});
-
-
-
+console.log('hello from index.js');
