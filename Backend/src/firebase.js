142b646f08173e71498e19aeb92131470555a9cd
Steven Uy <steven.uy10@gmail.com>
2024-06-19 14:45:12 +0800

    able to write data into the database

---
+++ Backend/src/firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCgQq7oJBQmva2xKUj_TuLsB7CS4ZunoZY",
+   authDomain: "skincare-analyzer.firebaseapp.com",
+   projectId: "skincare-analyzer",
+   storageBucket: "skincare-analyzer.appspot.com",
+   messagingSenderId: "942303322650",
+   appId: "1:942303322650:web:e0fb8d0b1da5272ce867a3",
+   measurementId: "G-92QQXCXELJ"
+ };
+
+ //initialize firebase app
+const app = initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+//add data to db
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
