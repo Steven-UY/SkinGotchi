ad36b2e8474829ec4c4153a0e3969771f1993061
Steven Uy <steven.uy10@gmail.com>
2024-06-25 11:00:05 +0800

    reset the test branch

--- Backend/firebase.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import config from './config.js';
-
-//initialize firebase app with config from env file
-const firebase = initializeApp(config.firebaseConfig);
-
-export default firebase;
