c3523fca8c8b5926c5685d2a0726b6d2791ca766
Steven Uy <steven.uy10@gmail.com>
2024-07-03 17:09:39 +0800

    signup functionality with auth

--- Backend/server.js
+++ Backend/server.js
@@ -98,6 +98,77 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
+const db = admin.firestore();
+
+//create user(with auth)
+app.post('/signup', async(req, res) => {
+    try{
+        const { email, password, firstName, lastName } = req.body;
+
+        const userResponse = await admin.auth().createUser({
+            email: email,
+            password: password,
+            emailVerified: false,
+            disabled: false
+        });
+
+        const userJson = {
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+        };
+        await db.collection("users").doc(userResponse.uid).set(userJson);
+
+        res.json(userResponse);
+    } catch(error){
+        res.status(400).send(error);
+    }
+});
+
+//creates user
+app.post('/create', async(req, res) => {
+    try{
+        const id = req.body.email;
+        console.log(req.body);
+        const userJson = {
+            email : req.body.email,
+            firstName : req.body.firstName,
+            lastName : req.body.lastName 
+        };
+        const response = await db.collection("users").add(userJson);
+        res.send(response);
+    } catch(error) {
+        res.send(error);
+    }
+})
+
+//reads all the documents
+app.get('/read/all', async(req, res) => {
+    try{
+        const usersRef = db.collection("users");
+        const response = await usersRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.send(responseArr);
+    } catch(error) {
+        res.send(error);
+    }
+})
+
+//read particular id
+app.get('/read/:id', async (req, res) => {
+    try{
+        const usersRef = db.collection("users").doc(req.params.id);
+        const response = await usersRef.get();
+        res.send(responseArr);
+    } catch(error) {
+        res.send(error);
+    }
+})
+
+//creates user with auth credentials
 app.post('/signup', async(req, res) => {
     console.log(req.body);
     const user = {
@@ -113,10 +184,39 @@ app.post('/signup', async(req, res) => {
     res.json(userResponse);
 })
 
-// set post and listen for our requests
+//update user
+app.post('/update', async(req, res) => {
+    try{
+        const id = req.body.id;
+        const newFirstName = "jeff";
+        const userRef = await db.collection("users").doc(id)
+        .update({
+            firstName: newFirstName
+        });
+        res.send(response);
+    } catch(error) {
+        res.send(error);
+    }
+})
+
+//delete document based on id
+app.delete('/delete/:id', async(req, res) => {
+    try{
+        const response = await db.collection("users").doc(req.params.id).delete();
+        res.send(response);
+    } catch(error) {
+        res.send(error);
+    }
+})
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}.`);
 });
 
+/*
+1) whenever we create a user it's with auth(done)
+2) change the fields to match the data that we want
+3) implement signin functionality with JWTs
+*/
