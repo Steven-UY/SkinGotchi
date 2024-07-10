9875c795cec33d828c56b89a2001448a1bb06c29
Steven Uy <steven.uy10@gmail.com>
2024-07-10 12:15:54 +0800

    webpack dev server setup

--- Frontend/src/firebaseInit.mjs
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import firebaseConfig from './firebaseConfig.mjs';
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { app, auth };
